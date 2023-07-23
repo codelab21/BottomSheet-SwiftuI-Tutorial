783edfe2b66e1baaded6f6da6486c34887f76a42
codelab21 <codelab21@outlook.com>
2023-07-23 18:49:12 +0300

    Initial Commit

---
+++ BottomSheetTutorial/BottomSheetTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetTutorialApp.swift
+//  BottomSheetTutorial
+//
+//  Created by Eymen on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
